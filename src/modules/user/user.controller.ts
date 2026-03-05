3d52be6751c628603c36e94a0b473e0477c08b03
vietanh-dev-stack <ngvietanh.08112003@gmail.com>
2026-03-05 22:22:31 +0700

    decorator with api

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -1,11 +1,27 @@
-import { Controller, Get } from '@nestjs/common';
+/* eslint-disable @typescript-eslint/no-unsafe-return */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+import { Body, Controller, Get, Param, Post, Query } from '@nestjs/common';
 import { UserService } from 'src/modules/user/user.service';
 
 @Controller('users')
 export class UserController {
   constructor(private readonly userService: UserService) {}
   @Get()
-  index() {
-    return this.userService.getUsers();
+  index(@Query() query: any) {
+    return {
+      keyword: query.keyword,
+      category: query.category,
+    };
+  }
+
+  @Get('/:id')
+  find(@Param('id') id: string) {
+    return `user ${id}`;
+  }
+
+  @Post()
+  create(@Body() body: any) {
+    return body;
   }
 }
